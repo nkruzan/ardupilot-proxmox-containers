58c9269f8a5ebbf1957fe27477c3118682c49dfc
Nicholas Kruzan <nickkruzan@gmail.com>
2021-10-26 15:39:53 -0500

    add seperate script for custom build
    updates
    
    
    updates
    
    
    testing
    
    
    testing
    
    
    more testing
    
    
    updates
    
    
    more-updates
    
    
    more updates
    
    
    fix sed
    
    
    updates
    
    
    more cahnges

--- install-custom-build-server.sh
+++ install-custom-build-server.sh
@@ -1,55 +1,11 @@
 #!/bin/bash
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may run this script (it adds user)"
-	exit 2
-fi
+read -p "Enter username : " username
+read -s -p "Enter password : " password
+read -r -p "Enter dns name for server:" servername
 
-egrep "^$username" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-    echo "adding $username to sudoers..."
-    echo "$username  ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-    #we dont want the password to be in the logs...
-    export HISTIGNORE='*sudo -S*'
-
-    #user was successfully added continue here as new user
-    echo "$password" | sudo -S -i -u "$username" bash << EOF
-sudo apt clean
-sudo apt update
-sudo apt upgrade -y 
-mkdir base
-cd base
-#git clone https://github.com/ArduPilot/ardupilot.git
-git clone https://github.com/nkruzan/ardupilot.git
-cd ardupilot
-#git checkout pr/bullseye-install
-git submodule update --init --recursive
-Tools/environment_install/install-prereqs-ubuntu.sh -y
-#echo "Starting SITL"
-#cd ArduPlane
-#sim_vehicle.py -w
-EOF
-
-
-fi
-
-while true; do
-    read -r -p "Install Custom Build Server? (Y/N): " answer
-    case $answer in
-        [Yy]* ) read -r -p "Enter dns name for server:" servername
-		echo "$password" | sudo -S -i -u "$username" bash << EOF
+#we dont want the password to be in the logs...
+export HISTIGNORE='*sudo -S*'
+echo "$password" | sudo -S -i -u "$username" bash << EOF
 sudo apt install apache2 libapache2-mod-wsgi-py3 python3 python3-pip -y
 sudo pip3 install flask
 #/home/$username/base/ardupilot/Tools/scripts/configure-ci.sh 
@@ -83,19 +39,3 @@ sudo echo "       CustomLog ${APACHE_LOG_DIR}/access.log combined"  | sudo tee -
 sudo echo "</VirtualHost>"  | sudo tee -a /etc/apache2/sites-available/CustomBuild.conf
 sudo a2ensite CustomBuild.conf
 EOF
- 				break;;
-
-        [Nn]* ) break;;
-        * ) echo "Please answer Y or N.";;
-    esac
-done
-
-while true; do
-    read -r -p "Do you wish to reboot the system? Required for Custom Build to run(Y/N): " answer
-    case $answer in
-        [Yy]* ) reboot; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer Y or N.";;
-    esac
-done
-
